4a3dbcfb27d8d7f1f9336b9d8165f0f8654bda2b
Andrej Soldo <soldo.andrej@gmail.com>
2023-02-10 18:05:24 +0100

    Minor update on event bus on App.vue

--- wicked-balad-frontend/src/App.vue
+++ wicked-balad-frontend/src/App.vue
@@ -4,11 +4,18 @@ import { ref, provide, inject, onMounted } from "vue";
 import { storeToRefs } from "pinia";
 import Dungeon from "@/components/dungeon.vue";
 import { useCounterStore } from "@/stores/counter";
+import mitt from "mitt";
+const emitter = mitt();
 
+emitter.on("myevent", (value: any) => {
+  // *Listen* for event
+  console.log("myevent received!", `value: ${value}`);
+});
 const mystore = useCounterStore();
 const { count } = storeToRefs(mystore);
 const myValue = ref(333);
 const prov = provide("myValue", myValue);
+const prove = provide("emitter", emitter);
 console.log(myValue.value);
 console.log(count.value);
 </script>
--- wicked-balad-frontend/src/main.ts
+++ wicked-balad-frontend/src/main.ts
@@ -6,15 +6,15 @@ import App from "@/App.vue";
 import router from "@/router";
 
 import "@/assets/main.css";
-import mitt from "mitt";
-const emitter = mitt();
+// import mitt from "mitt";
+// const emitter = mitt();
 const app = createApp(App);
-app.provide("emitter", emitter);
+// app.provide("emitter", emitter);
 
 app.use(createPinia());
 app.use(router);
-emitter.on("myevent", (value: any) => {
-  // *Listen* for event
-  console.log("myevent received!", `value: ${value}`);
-});
+// emitter.on("myevent", (value: any) => {
+//   // *Listen* for event
+//   console.log("myevent received!", `value: ${value}`);
+// });
 app.mount("#app");
